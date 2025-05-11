ce81d2553fbc0566b7325734cfa01033e27b92b2
Gugustinette <mercier.augustin@outlook.fr>
2025-05-11 16:43:54 +0200

    chore: rename tests/data to fixtures

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,7 +3,7 @@ import { cli } from "../src/cli";
 
 describe("rundown", () => {
 	test("should run the file without watch", async () => {
-		const args = ["./test/data/index.data.ts"];
+		const args = ["./test/fixtures/index.data.ts"];
 		const result = await cli(args);
 		expect(result).toBeUndefined();
 	});
